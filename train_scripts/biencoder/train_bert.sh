fdd1facf2ac0f1921efdbf68837ef0066708639f
kaledai069 <parzival.cooler@gmail.com>
2023-08-14 00:10:33 +0545

    training params for kaggle env

--- train_scripts/biencoder/train_bert.sh
+++ train_scripts/biencoder/train_bert.sh
@@ -3,14 +3,14 @@ VALID_FILE=$2
 OUTPUT_DIR=$3
 
 python DPR/train_dense_encoder.py \
-	--batch_size 16 \
-	--dev_batch_size 16 \
+	--batch_size 64 \
+	--dev_batch_size 64 \
 	--sequence_length 32 \
 	--warmup_steps 1237 \
 	--max_grad_norm 2.0 \
 	--learning_rate 2e-05 \
 	--seed 1 \
-	--num_train_epochs 20 \
+	--num_train_epochs 15 \
 	--eval_per_epoch 3 \
 	--do_lower_case \
 	--encoder_model_type hf_bert \
