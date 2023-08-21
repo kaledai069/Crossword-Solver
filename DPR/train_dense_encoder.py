b1df79b238877869eacb2b7dbff0b4aff5f06cff
kaledai069 <parzival.cooler@gmail.com>
2023-08-21 20:42:46 +0545

    Testing scheduler state and optimizer state dicts

--- DPR/train_dense_encoder.py
+++ DPR/train_dense_encoder.py
@@ -568,9 +568,12 @@ class BiEncoderTrainer(object):
         if saved_state.optimizer_dict:
             logger.info("Loading saved optimizer state ...")
             self.optimizer.load_state_dict(saved_state.optimizer_dict)
+            print(self.optimizer)
 
         if saved_state.scheduler_dict:
             self.scheduler_state = saved_state.scheduler_dict
+            print(self.scheduler_state)
+            self.scheduler_state['_last_lr'] = self.scheduler_state['base_lrs']
 
 
 def _calc_loss(
