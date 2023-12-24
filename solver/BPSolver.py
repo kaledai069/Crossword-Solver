c0a0c2678891a7e0c8c64c091dd7aa26dfaa1958
kaledai069 <parzival.cooler@gmail.com>
2023-12-24 16:26:49 +0545

    tweaking higher word flips and lower threahold for improved grid score

--- solver/BPSolver.py
+++ solver/BPSolver.py
@@ -288,7 +288,7 @@ class BPSolver(Solver):
         for clue in uncertain_answers.keys():
             initial_word = uncertain_answers[clue]
             # print("INITIAL WORD: ", initial_word)
-            clue_flips = get_word_flips(initial_word, 20) # flip then segment
+            clue_flips = get_word_flips(initial_word, 30) # flip then segment
             # print(clue_flips)
             clue_positions = [key for key, value in self.crossword.variables.items() if value['clue'] == clue]
             # print(clue_positions)
@@ -464,7 +464,7 @@ class BPSolver(Solver):
                 # print('clue', self.crossword.variables[clue_index]['clue'])
             # print('original score:', original_grid_score, 'modified score:', modified_grid_score)
                 
-            if modified_grid_score - original_grid_score > 0.5:
+            if modified_grid_score - original_grid_score > 0.2:
                 # print('found a possible edit')
                 possible_edits.append((modified_grid, modified_grid_score, replacements))
             # print()
